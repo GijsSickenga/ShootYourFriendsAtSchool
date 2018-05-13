e9675dd45f70580596a15a3c634fc662529e82b6
GijsSickenga <gijssickenga@gmail.com>
2018-05-13 22:59:13 +0200

    werkt bijna

---
+++ Assets/GunUIComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class GunUIComponent : MonoBehaviour
+{
+    // The weight amount for the component.
+	[SerializeField]
+    private Text weightText;
+
+    // The slider that shows how much of the max weight is put into the component.
+	[SerializeField]
+    private Slider slider;
+
+	[SerializeField]
+	private VariableWeight weaponVariableType;
+    /// <summary>
+    /// The weapon variable associated with this UI element.
+    /// </summary>
+    public VariableWeight GetWeaponVariableType()
+	{
+		return weaponVariableType;
+	}
+
+	// Initialization.
+	private void Start()
+	{
+		slider.maxValue = weaponVariableType.maxedWeight;
+	}
+
+	/// <summary>
+	/// Sets the weight text parameter of the component.
+	/// </summary>
+	/// <param name="newWeight">The new weight to set the weight text to.</param>
+    public void SetWeight(float newWeight)
+    {
+		// Set weight text and slider value.
+        weightText.text = newWeight.ToString("0.0") + "/" + weaponVariableType.maxedWeight.ToString("0.0");
+		slider.value = newWeight;
+    }
+}
---
+++ Assets/Scripts/GUI/GunUIParent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunUIParent : MonoBehaviour
+{
+    /// <summary>
+    /// A list of gun UI components contained in this gun UI parent.
+    /// </summary>
+    [SerializeField]
+    private List<GunUIComponent> componentsList = new List<GunUIComponent>();
+
+    /// <summary>
+    /// A dictionary containing all gun UI components with their
+    /// corresponding gun variable types.
+    /// </summary>
+    [HideInInspector]
+    public Dictionary<string, GunUIComponent> components = new Dictionary<string, GunUIComponent>();
+
+    // Initialization.
+    private void Awake()
+    {
+        foreach (GunUIComponent component in componentsList)
+        {
+            // Add the component to the dictionary, along with its weapon
+            // variable type as its ID.
+            components.Add(component.GetWeaponVariableType().variableName, component);
+        }
+    }
+}
