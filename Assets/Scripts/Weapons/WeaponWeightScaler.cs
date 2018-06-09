712bef8479be340d98897089e92e1ec930fd4d71
Ian van Heeren <ianvanheeren@hotmail.com>
2018-06-09 17:43:13 +0200

    hgfjouewfgojis jaaaaaaaa

--- Assets/Scripts/Weapons/WeaponWeightScaler.cs
+++ Assets/Scripts/Weapons/WeaponWeightScaler.cs
@@ -8,23 +8,33 @@ public class WeaponWeightScaler : MonoBehaviour {
 	 *	There are a few modes, like:
 	 *  Base on kill/death ratio
 	 *	Base on kills in context with total kills
+	 *	Base on kills away from an average
 	 */
 
 	private enum WeightBasedOn 
 	{
 		killDeathRatio,
-		totalKills
+		totalKills,
+		averageRange
 	}
 
 	[SerializeField]
-	private WeightBasedOn weightBasedOn = WeightBasedOn.totalKills;
+	private WeightBasedOn weightBasedOn = WeightBasedOn.averageRange;
 
 	[SerializeField]
 	// When scale is calculated, adds this minScale to calculated scale
-	private float minScale = 0;
+	private float minScale = 0.5f;
 	[SerializeField]
 	// Maximum scale to be used
 	private float maxScale = 2;
+
+	[SerializeField]
+	// Middle scale point. This should be the perfect balance between good and bad players.
+	private float scaleMiddle = 1;
+
+	// For use when needing a value under and above averages
+	[SerializeField]
+	private readonly int killScaleRange = 10;
 
 	// Calculates weights for each player
 	public void CalculateWeights()
@@ -41,6 +51,17 @@ public class WeaponWeightScaler : MonoBehaviour {
 			totalKills += killsArray[i];
 			totalDeaths += deathsArray[i];
 		}
+
+		// Check the amount of people actually playing.
+		int playingPlayers = 0;
+		foreach(LocalPlayerController player in GameData.players)
+		{
+			// oh god why
+			if(player != null)
+				playingPlayers++;
+		}
+		// Lastly, calculate the average
+		int avgKills = (int)Mathf.Ceil((float)totalKills / (float)playingPlayers);
 
 		if(totalKills <= 0 || totalDeaths <= 0)
 			return;
@@ -67,6 +88,10 @@ public class WeaponWeightScaler : MonoBehaviour {
 						playerData.weaponWeightScalar = CalculateKillDeathScale(playerKills, playerDeaths);
 						break;
 
+					case WeightBasedOn.averageRange:
+						playerData.weaponWeightScalar = CalculateAvgKillScale(playerKills, avgKills);
+						break;
+
 					default:
 						break;
 				}
@@ -85,7 +110,7 @@ public class WeaponWeightScaler : MonoBehaviour {
 		// Inverse the percentage of kills player has in context of total number of kills,
 		// then multiply that by the maximum scale and add the minimum scale.
 		// Get lower scale for more kills
-		return (Mathf.Abs((playerKills / totalKills) - 1) * (maxScale - minScale)) + minScale;
+		return ((1 - (playerKills / totalKills)) * (maxScale - minScale)) + minScale;
 	}
 
 	private float CalculateKillDeathScale(float kills, float deaths)
@@ -100,5 +125,54 @@ public class WeaponWeightScaler : MonoBehaviour {
 		// Inverse K/D ratio, then multiply by maxScale and add minScale. 
 		// Get lower scale for higher K/D ratio.
 		return (Mathf.Abs((kills / deaths) - 1) * (maxScale - minScale)) + minScale;
+	}
+
+	private float CalculateAvgKillScale(int playerKills, int avgKills)
+	{
+		int nKillsFromAvg = avgKills - playerKills;
+
+		// If literally the average, return middle point 
+		if(nKillsFromAvg == 0)
+		{
+			return scaleMiddle;
+		}
+		else if(nKillsFromAvg > 0)
+		{
+			// Check the difference between middlepoint scale and the minimum scale
+			// Example: kills from average = 4, range = 10, minimum scale = 0.5, scale middlepoint = 1
+			// This results in playerScale = 0.5
+			float playerScale = scaleMiddle - minScale;
+
+			// Multiply this by the inverse of player's kills multiplied by the average from the range
+			// Example: 0.5 * (1 - (4 / 10)) = 0.3
+			playerScale *= 1 - (nKillsFromAvg / killScaleRange);
+
+			// If the player scale is under 0, return the minimal scale
+			// Example: skip this
+			if(playerScale < 0)
+				return minScale;
+
+			// Return this plus the minimal scale
+			// Example: returned will be 0.3 + 0.5 = 0.7
+			return playerScale + minScale;
+		}
+		else
+		{
+			// Opposite of above
+			// Example: kills from average = 4, range = 10, maximum scale = 2, scale middlepoint = 1
+			// This results in playerScale = 1
+			float playerScale = maxScale - scaleMiddle;
+
+			// Example: 1 * (4 / 10) = 0.4
+			playerScale *= nKillsFromAvg / killScaleRange;
+
+			// Check if playerScale is higher than 1, because it would've exceeded the range. Return maximum
+			// Example: skip this
+			if(playerScale > 1)
+				return maxScale;
+
+			// Example: 0.4 + 1 = 1.4
+			return playerScale + scaleMiddle;
+		}
 	}
 }
